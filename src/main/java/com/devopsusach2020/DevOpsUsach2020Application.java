a21134964cdd58b5e06a50358dcc3dedcb133dc9
Diego Tapia R <dftapiaru@falabella.cl>
2022-01-15 11:26:29 -0300

    add dummies test

--- src/main/java/com/devopsusach2020/DevOpsUsach2020Application.java
+++ src/main/java/com/devopsusach2020/DevOpsUsach2020Application.java
@@ -6,12 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DevOpsUsach2020Application {
 
-	@Test void contextLoads0() {
+	@Test 
+	void contextLoads0() {
 		String correcto = "hi";
 		Assertions.assertThan(correcto).isEqualTo("hi");
 	}
 
-	@Test void contextLoads1() {
+	@Test 
+	void contextLoads1() {
 		String correcto = "hi";
 		Assertions.assertThan(correcto).isEqualTo("hi");
 	}
